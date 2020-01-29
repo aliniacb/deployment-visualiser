9d1b7fbc49ceee25fefe734898a95400e2e81aa4
Alin Iacob <aliniacb@gmail.com>
2020-01-29 11:28:03 +0000

    initial

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,17 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+</script>
+
+<style lang="sass">
 </style>
