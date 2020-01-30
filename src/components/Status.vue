7da2b8fb6df124949384283e7217513079fb9e3c
Alin Iacob <aliniacb@gmail.com>
2020-01-30 08:59:57 +0000

    adds line draws

---
+++ src/components/Status.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="{'is-hidden': !hasPendingEvents }" class="column is-12">
+    <div :class="{'shrinked': !hasPendingEvents }" class="box service-status">
+      <h4 class="title is-4 has-text-centered">Status updates</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Status',
+  props: [ 'data' ],
+  methods: {
+
+  },
+
+  computed: {
+    hasPendingEvents() {
+      return this.data.find(e => e.running)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .service-status {
+    height: 300px;
+    box-shadow: -5px -2px 20px 0px rgba(0, 0, 0, 0.1);
+    transform: scale(1);
+
+    transition: all .3s ease;
+
+    &.shrinked {
+      height: 0;
+
+      box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+      transform: scale(0);
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,15 @@
   <section class="hero is-fullheight">
     <div class="hero-body is-small">
       <div class="container is-fluid">
-        <div class="columns">
+        <div class="columns is-vcentered is-fullheight">
           <div class="column is-5">
             <div class="columns is-multiline">
-              <Team v-for="(team, teamName, i) in teams" :data="team" :teamName="teamName" :key="i"/>
+              <Team :ref="`t-${i}`" v-for="(team, teamName, i) in teams" :data="team" :teamName="teamName" :key="i"/>
+            </div>
+          </div>
+          <div class="column is-6 is-offset-1">
+            <div class="columns is-multiline is-vcentered is-fullheight">
+              <Status :ref="`s-${i}`" v-for="(team, teamName, i) in teams" :data="team" :key="i" />
             </div>
           </div>
         </div>
@@ -17,37 +22,41 @@
 
 <script>
 import Team from '@/components/Team.vue'
+import Status from '@/components/Status.vue'
 
 export default {
   name: 'home',
   components: {
-    Team
+    Team,
+    Status
   },
 
   data() {
     return {
+      ctx: null,
       teams: {
         'Core services': [
-          { name: "service1" },
-          { name: "service2" },
-          { name: "service2" }
+          { name: 'service1', running: true },
+          { name: 'service2' },
+          { name: 'service2' }
         ],
         'Euro': [
-          { name: "service1" },
-          { name: "service2" },
-          { name: "service2" }
+          { name: 'service1' },
+          { name: 'service1', running: true },
+          { name: 'service2' },
+          { name: 'service2' }
         ],
         'Platform': [
-          { name: "service1" },
-          { name: "service2" }
+          { name: 'service1' },
+          { name: 'service2' }
         ],
         'International': [
-          { name: "service1" },
+          { name: 'service1' },
         ],
         'Uk': [
-          { name: "service1" },
-          { name: "service2" },
-          { name: "service2" }
+          { name: 'service1' },
+          { name: 'service2' },
+          { name: 'service2' }
         ],
       }
     }
@@ -55,6 +64,54 @@ export default {
   mounted() {
     this.$refs.c.width = window.innerWidth
     this.$refs.c.height = window.innerHeight
+    this.ctx = this.$refs.c.getContext('2d')
+
+    this.tick()
+  },
+
+  methods: {
+    getDrawCoord() {
+      let coord = []
+
+      for (let i = 0; i < Object.keys(this.teams).length; i += 1) {
+        let teamSquareCoord = this.$refs[`t-${i}`][0].$el.getBoundingClientRect()
+        let statusSquareCoord = this.$refs[`s-${i}`][0].$el.getBoundingClientRect()
+
+
+        let teamSquareCenterY = teamSquareCoord.top + teamSquareCoord.height / 2
+        let teamSquareRightX = teamSquareCoord.left + teamSquareCoord.width
+
+        let statusSquareCenterY = statusSquareCoord.top + statusSquareCoord.height / 2
+        let statusSquareRightX = statusSquareCoord.left
+
+        coord.push({
+          from: { x: teamSquareRightX, y: teamSquareCenterY },
+          to: { x: statusSquareRightX, y: statusSquareCenterY }
+        })
+      }
+
+      return coord
+    },
+    render() {
+      this.ctx.beginPath()
+      this.ctx.lineWidth = 5
+      this.ctx.lineJoin = 'round'
+      this.ctx.lineCap = 'round'
+      for (let line of this.getDrawCoord()) {
+        if (line.to.x !== 0) {
+          this.ctx.moveTo(line.from.x, line.from.y)
+          this.ctx.lineTo(line.from.x + 30, line.from.y)
+          this.ctx.lineTo(line.to.x - 30, line.to.y)
+          this.ctx.lineTo(line.to.x, line.to.y)
+        }
+      }
+      this.ctx.stroke()
+    },
+    tick() {
+      window.requestAnimationFrame(this.tick.bind(this))
+      this.ctx.clearRect(0, 0, this.$refs.c.width, this.$refs.c.height)
+      this.render()
+    }
   }
 }
 </script>
