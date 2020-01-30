60c5f7daebd2047439e0bf2cd350e3da1741841e
Alin Iacob <aliniacb@gmail.com>
2020-01-30 12:00:39 +0000

    remove console log

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -62,7 +62,6 @@ export default {
       const i = Math.floor(Math.random() * Object.keys(this.teams).length)
       let team = this.teams[Object.keys(this.teams)[i]][0]
       team.deploymentInProgress === true ? team.deploymentInProgress = false : team.deploymentInProgress = true
-      console.log(i, team)
       this.updateCanvasSize()
     },
 
