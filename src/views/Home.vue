669115da9cd7662888b2c853a8ec36722d637ed8
Alin Iacob <aliniacb@gmail.com>
2020-01-30 12:51:57 +0000

    more randomness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@ export default {
 
     this.tick()
 
-    setInterval(this.fetchData.bind(this), 3000)
+    setInterval(this.fetchData.bind(this), 5000)
   },
 
   methods: {
@@ -60,7 +60,7 @@ export default {
       this.mapDataToTeams()
 
       const i = Math.floor(Math.random() * Object.keys(this.teams).length)
-      let team = this.teams[Object.keys(this.teams)[i]][0]
+      let team = this.teams[Object.keys(this.teams)[i]][Math.floor(Math.random() * Object.keys(this.teams)[i].length)]
       team.deploymentInProgress === true ? team.deploymentInProgress = false : team.deploymentInProgress = true
       this.updateCanvasSize()
     },
